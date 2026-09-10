884aa359d3c89ce89c8351197886483aa231ccb7
catalinamba <mbacatalina@gmail.com>
2026-09-10 18:06:28 +0200

    cambios 10 sept

--- UD1/ProcesoEjemplo.java
+++ UD1/ProcesoEjemplo.java
@@ -4,14 +4,12 @@ public class ProcesoEjemplo {
 
     public static void main(String[] args) throws Exception {
         
-        
+        // ejemplo proceso abrir calculadora
         ProcessBuilder pb = new ProcessBuilder("cmd","/c", "start calc");
 
         Process p = pb.start();
 
-        p.waitFor();
-
-        
+        p.waitFor(); 
         
     }
 }
